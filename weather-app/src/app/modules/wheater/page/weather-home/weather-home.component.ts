319588961ffc128fde8fa066df15caa18539443e
Gabriel Klein <gabriel.klein@philips.com>
2024-07-22 14:04:04 -0300

    Adicionado o primeiro form

--- weather-app/src/app/modules/wheater/page/weather-home/weather-home.component.ts
+++ weather-app/src/app/modules/wheater/page/weather-home/weather-home.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { WeatherData } from 'src/app/models/interfaces/WeatherData';
+import { Subject, takeUntil } from 'rxjs';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: []
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
+  initialCityName = 'Itapiranga';
+  weatherDatas!: WeatherData;
+  searchIcon = faMagnifyingGlass;
 
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.getWeatherData(this.initialCityName);
+  }
+
+  getWeatherData(cityName: string): void {
+    this.weatherService.getWeatherData(cityName)
+    .pipe(takeUntil(this.destroy$))
+    .subscribe({
+      next: (response) => {
+        response && (this.weatherDatas = response);
+        console.log(this.weatherDatas);
+      },
+      error: (error) => console.log(error),
+    });
+  }
+
+  onSubmit(): void {
+    this.getWeatherData(this.initialCityName);
+    this.initialCityName = '';
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
