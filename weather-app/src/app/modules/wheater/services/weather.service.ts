2a64ad9afd6e5de508b03462057641325f3f740a
Gabriel Klein <gabriel.klein@philips.com>
2024-07-22 12:21:07 -0300

    Adicionado o services e o componente home

---
+++ weather-app/src/app/modules/wheater/services/weather.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  private apiKey = '9b344a88b57f230195210d1ebd880e89';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  getWeatherData(cityName: string): Observable<any> {
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`, {});
+  }
+}
